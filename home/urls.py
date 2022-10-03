e824fa99e46e45f1f99e78bf2c081e8350d5cfe6
Adnan Muhtadi <adnanmuhtadi@hotmail.com>
2022-10-03 22:49:30 +0100

    Other home page pages

--- home/urls.py
+++ home/urls.py
@@ -3,5 +3,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='home')
+    path('', views.index, name='home'),
+    path('about/', views.about, name='about'),
+    path('consultation/', views.consultation, name='consultation'),
+    path('contact/', views.contact, name='contact'),
+    path('process/', views.process, name='process')
 ]
--- home/views.py
+++ home/views.py
@@ -7,3 +7,35 @@ def index(request):
     """ A view to render the index page """
 
     return render(request, 'home/index.html')
+
+
+def about(request):
+    """
+    A view to return the process page
+    """
+
+    return render(request, 'home/about.html')
+
+
+def consultation(request):
+    """
+    A view to return the process page
+    """
+
+    return render(request, 'home/consultation.html')
+
+
+def contact(request):
+    """
+    A view to return the process page
+    """
+
+    return render(request, 'home/contact.html')
+
+
+def process(request):
+    """
+    A view to return the process page
+    """
+
+    return render(request, 'home/process.html')
